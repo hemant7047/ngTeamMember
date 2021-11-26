8caac7f2128e2b03aaa5cd8bb72a8dd56c9ecc3f
Hemant <hsr7047@gmail.com>
2021-11-26 21:58:00 +0530

    TeamMember

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teamApp';
+  newMemberName='';
+  members:string[]=[];
+  errorMessage='';
+  
+
+
+  onInput(member:string){
+    this.newMemberName=member;
+    // console.log(this.newMemberName)
+  }
+
+  addMember(){
+    if(!this.newMemberName){
+      this.errorMessage=("Name can't be empty");
+      return;
+    }
+    this.members.push(this.newMemberName);
+    this.newMemberName='';
+    this.errorMessage=("");
+    // console.log(this.members);
+    // console.log('Button Is clicked')
+  }
+
 }
