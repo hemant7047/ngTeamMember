b77672b39a0d336fa97aef7446b317577e063cfc
Hemant <hsr7047@gmail.com>
2021-11-29 14:50:43 +0530

    Completed

---
+++ src/app/team/team.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css']
+})
+export class TeamComponent implements OnInit {
+
+  @Input() team:string[]=[];
+  @Input() index=0;
+  currentDate= new Date();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
